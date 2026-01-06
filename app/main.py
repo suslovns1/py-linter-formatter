d707a3c88d15f8ade0df3160ce05cbe9b067d27d
Nikita Suslov <nikitasuslov.manage@gmail.com>
2026-01-06 07:03:12 +0200

    Resolving issues related to non-compliance with flake8 rules

--- app/main.py
+++ app/main.py
@@ -8,14 +8,12 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "passed" if len(errors) == 0 else "failed"
     }
-
 
 
 def format_linter_report(linter_report: dict) -> list:
